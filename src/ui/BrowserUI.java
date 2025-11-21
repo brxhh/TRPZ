144a2e7c28d694c568e2dd494bc792260a4ef1af
stanislav <ctac.samoylenko@gmail.com>
2025-11-21 21:18:47 +0100

    feat: implement Proxy pattern for caching web pages (Lab 4)

--- src/ui/BrowserUI.java
+++ src/ui/BrowserUI.java
@@ -1,8 +1,10 @@
 package ui;
+
 import database.DatabaseContext;
 import entities.Bookmark;
 import entities.HistoryItem;
-import modules.NetworkModule;
+import modules.INetworkModule;
+import modules.CachingProxy;
 import modules.RenderingEngine;
 import repositories.BookmarkRepository;
 import repositories.HistoryRepository;
@@ -10,26 +12,27 @@ import repositories.HistoryRepository;
 public class BrowserUI {
     private BookmarkRepository bookmarkRepo;
     private HistoryRepository historyRepo;
-    private NetworkModule network;
+    private INetworkModule network;
     private RenderingEngine renderer;
-
     private String currentUrl;
 
     public BrowserUI() {
         DatabaseContext db = new DatabaseContext();
         this.bookmarkRepo = new BookmarkRepository(db);
         this.historyRepo = new HistoryRepository(db);
-        this.network = new NetworkModule();
+        this.network = new CachingProxy();
         this.renderer = new RenderingEngine();
         System.out.println("Браузер запущено.");
     }
+
     public void navigateTo(String url) {
         System.out.println("\n--- Сценарій: Відкриття " + url + " ---");
         this.currentUrl = url;
-        String html = network.sendGetRequest(url);
+        String html = network.sendRequest(url);
         renderer.render(html);
         historyRepo.add(new HistoryItem(url));
     }
+
     public void addToBookmarks() {
         System.out.println("\n--- Сценарій: Додавання закладки ---");
         if (currentUrl != null) {
